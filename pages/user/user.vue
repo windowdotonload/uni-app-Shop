bd455d4a000b9d6ec944db9d384ba75d9f2e7636
windowdotonload <275981471@qq.com>
2020-12-09 09:28:26 +0800

    mod

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,22 +1,81 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: windowdotonload
+-->
 <template>
-	<view>
-		<h1>car</h1>
-	</view>
+  <view class="userContiner">
+    <view class="topBac">
+      <image
+        class="bac"
+        mode="scaleToFill"
+        :src="userinfo.userInfo.avatarUrl"
+      ></image>
+      <image
+        mode="aspectFit"
+        class="showAvatar"
+        :src="userinfo.userInfo.avatarUrl"
+      ></image>
+    </view>
+
+    <view class="Menu"> </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+  data() {
+    return {
+      userinfo: {},
+    };
+  },
+  onShow() {
+    uni.getUserInfo({
+      provider: "weixin",
+      success: (infoRes) => {
+        console.log(infoRes);
+        this.userinfo = infoRes;
+        uni.setStorageSync("userinfo", infoRes);
+      },
+    });
+  },
+  methods: {},
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.userContiner {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.topBac {
+  width: 100%;
+  height: 50%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  .bac {
+    width: 100%;
+    height: 100%;
+    filter: blur(10px);
+  }
+  .showAvatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-55%);
+  }
+}
+.Menu {
+  width: 98%;
+  height: 100px;
+  background: red;
+  position: relative;
+  margin-top: -180px;
+}
 </style>

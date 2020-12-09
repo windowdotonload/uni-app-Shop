21169a12a347c32e1e961e303cfbc625b7b6ed68
windowdotonload <275981471@qq.com>
2020-12-09 14:51:48 +0800

    mod

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -3,6 +3,11 @@
  * @version: 
  * @Author: windowdotonload
 -->
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: windowdotonload
+-->
 <template>
   <view class="userContiner">
     <view class="topBac">
@@ -18,7 +23,57 @@
       ></image>
     </view>
 
-    <view class="Menu"> </view>
+    <view class="Menu">
+      <view class="collect">
+        <view class="showItem">
+          <p>0</p>
+          <p>收藏的店铺</p>
+        </view>
+        <view class="showItem">
+          <p>0</p>
+          <p>收藏的店铺</p>
+        </view>
+        <view class="showItem">
+          <p>0</p>
+          <p>收藏的店铺</p>
+        </view>
+        <view class="showItem">
+          <p>0</p>
+          <p>收藏的店铺</p>
+        </view>
+      </view>  
+      <view class="order">
+        <p class="myorder">我的订单</p>
+        <view class="orderDetail">
+          <view class="detailItem">
+            <image src='../../static/userorder.svg'></image>
+            <p>全部订单</p>
+          </view>
+          <view class="detailItem">
+            <image src='../../static/userpay.svg'></image>
+            <p>待付款</p>
+          </view>
+          <view class="detailItem">
+            <image src='../../static/userrec.svg'></image>
+            <p>待收货</p>
+          </view>
+          <view class="detailItem">
+            <image src='../../static/refund.svg'></image>
+            <p>退款/退货</p>
+          </view>
+        </view>
+      </view>
+      <view class="address">
+        <p>收货地址管理</p>
+      </view>
+      <view class="other">
+        <ul>
+          <li>联系客服</li>
+          <li>关于我们</li>
+          <li>公司地址</li>
+        </ul>
+      </view>
+     </view>
   </view>
 </template>
 
@@ -72,10 +127,95 @@ export default {
   }
 }
 .Menu {
-  width: 98%;
+  width: 96%;
   height: 100px;
-  background: red;
   position: relative;
-  margin-top: -180px;
+  margin-top: -150px;
+  border-radius: 5px;
+  .collect{
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    background: #f5f6fa;
+    .showItem{
+      width: 60px;
+      height: 50px;
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 5px;
+      margin-bottom: 5px;
+      p{
+        width: 60px;
+        font-size: 12px;
+        text-align: center;
+        &:nth-child(1){
+          color:red
+        }
+      }
+      
+    }
+  }
+  .order{
+    background: #f5f6fa;
+    .myorder{
+      padding-left: 5px;
+      padding-top: 5px;
+      margin-top: 10px;
+      height: 20px;
+      line-height: 20px;
+      color:rgb(197, 196, 192);
+    };
+    .orderDetail{
+      margin-top: 10px;
+      padding-top: 10px;
+      border-top: 1px solid rgb(167, 166, 166);
+      display: flex;
+      justify-content: space-between;
+    }
+    .detailItem{
+      width: 60px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      margin-bottom: 5px;
+      image{
+        width: 30px;
+        height: 30px;
+      }
+      p{
+        width: 60px;
+        font-size: 12px;
+        text-align: center;
+      }
+    }
+  }
+  .address{
+    width: 100%;
+    height: 50px;
+    margin-top: 10px;
+    padding-left: 10px;
+    line-height: 50px;
+    background: #f5f6fa;
+  }
+  .other{
+    margin-top: 10px;
+    margin-bottom: 30px;
+    background: #f5f6fa;
+    ul,li{
+      list-style: none;
+    }
+    li{
+      padding-left: 10px;
+      width: 100%;
+      height:30px;
+      line-height: 30px;
+      border-bottom: 1px solid rgb(175, 150, 150);
+      &:nth-child(3){
+        border: none;
+      }
+    }
+  }
 }
+
 </style>
